ac7655677df23f33e7be04707e985e905ec149bb
nasirjd <jafarzadeh.nasir@gmail.com>
2022-12-05 15:33:16 +0330

    Fixing Login Problem

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -23,9 +23,9 @@ router.get("/seed", asyncHandler(
 router.post("/login", asyncHandler(
   async (req, res) => {
     const {email, password} = req.body;
-    const user = await UserModel.findOne({email , password});
-      
-     if(user) {
+    const user = await UserModel.findOne({email});
+  
+     if(user && (await bcrypt.compare(password,user.password))) {
       res.send(generateTokenReponse(user));
      }
      else{
